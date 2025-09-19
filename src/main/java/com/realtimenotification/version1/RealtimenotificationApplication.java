41fb58d970108b5cb3f607634bd1daac6a9d5853
Claudio <marCoderApp>
2025-09-18 21:25:16 -0300

    First commit

---
+++ src/main/java/com/realtimenotification/version1/RealtimenotificationApplication.java
@@ -0,0 +1,13 @@
+package com.realtimenotification.version1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimenotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimenotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realtimenotification/version1/RealtimenotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimenotification.version1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimenotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
